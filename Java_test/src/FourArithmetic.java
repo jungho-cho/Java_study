0ed1c6da26369f2b29b96892cc9b62eb4de6b15c
myjun <myjun@DESKTOP-8B0T1DS.fritz.box>
2018-10-29 21:34:14 +0100

    added FourArithmetic class

---
+++ Java_test/src/FourArithmetic.java
@@ -0,0 +1,32 @@
+
+public class FourArithmetic {
+	public static void main(String args[])
+	{
+		int a = 10;
+		int b = 5;
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		
+		System.out.println(7 % 3);
+		System.out.println(3 % 7);
+		
+		int i = 0;
+		int j = 10;
+		i++;
+		j--;
+		
+		System.out.println(i);
+		System.out.println(j);
+		
+		i = 0;
+		System.out.println(i++);
+		System.out.println(i);
+		
+		i=0;
+		System.out.println(++i);
+		System.out.println(i);
+	}
+}
